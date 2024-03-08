bd073701aabd4d9831f05dbad000eea4fba15b30
Ameer Sobih <54002783+AmeerSDev@users.noreply.github.com>
2024-03-08 15:04:16 +0200

    adding Recipe model + related

---
+++ Recipe-App-WPF/Model/IngredientModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recipe_App_WPF.Model
+{
+    public class IngredientModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Recipe-App-WPF/Model/RecipeModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recipe_App_WPF.Model
+{
+    public class RecipeModel
+    {
+        public string Title { get; set; }
+        public int TimeMinutes { get; set; }
+        public float Price { get; set; }
+        public string Link { get; set; }
+        public List<TagModel> Tags { get; set; }
+        public List<IngredientModel> Ingredients { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+    }
+}
